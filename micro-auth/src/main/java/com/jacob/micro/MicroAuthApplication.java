bf82c5ed8ace33f81ef76bb46a47412755535fcc
jdw-art <jdw17733356203@icloud.com>
2024-03-18 19:06:58 +0800

    build(micro-auth):micro-auth module starter

--- micro-auth/src/main/java/com/jacob/micro/MicroAuthApplication.java
+++ micro-auth/src/main/java/com/jacob/micro/MicroAuthApplication.java
@@ -2,8 +2,12 @@ package com.jacob.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.jacob.micro")
 public class MicroAuthApplication {
 
     public static void main(String[] args) {
