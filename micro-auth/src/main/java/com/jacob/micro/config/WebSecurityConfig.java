5b830274d5b51d153d52283497425e921ac95b75
jdw-art <jdw17733356203@icloud.com>
2024-03-18 19:11:31 +0800

    build(micro-auth):SpringSecurity related configuration

---
+++ micro-auth/src/main/java/com/jacob/micro/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.jacob.micro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @Author: Jacob
+ * @Description: SpringSecurity相关配置
+ * @Date: 2024/3/18 19:10
+ * @Version: 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
