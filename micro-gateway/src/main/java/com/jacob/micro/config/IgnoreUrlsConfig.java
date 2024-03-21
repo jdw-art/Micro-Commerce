79f8815ef7ad7ac668ca97012ea44950c7b3c3e5
jdw-art <jdw17733356203@icloud.com>
2024-03-21 22:03:37 +0800

    build(micro-gateway):Gateway whitelist configuration

---
+++ micro-gateway/src/main/java/com/jacob/micro/config/IgnoreUrlsConfig.java
@@ -0,0 +1,22 @@
+package com.jacob.micro.config;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author: Jacob
+ * @Description: 网关白名单配置
+ * @Date: 2024/3/21 22:00
+ * @Version: 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+@ConfigurationProperties(prefix="secure.ignore")
+public class IgnoreUrlsConfig {
+    private List<String> urls;
+}
