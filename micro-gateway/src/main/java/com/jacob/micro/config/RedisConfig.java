eec50101bdd697181c97561693edd7bc7f853627
jdw-art <jdw17733356203@icloud.com>
2024-03-21 22:03:52 +0800

    build(micro-gateway):Redis related configuration

---
+++ micro-gateway/src/main/java/com/jacob/micro/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.jacob.micro.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Jacob
+ * @Description: Redis相关配置
+ * @Date: 2024/3/21 22:01
+ * @Version: 1.0
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig{
+}
