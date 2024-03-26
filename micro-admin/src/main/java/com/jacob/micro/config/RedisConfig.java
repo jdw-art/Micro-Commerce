c7123fdfd7155534c32b4f5c84654c0f7b865ba4
jdw-art <jdw17733356203@icloud.com>
2024-03-26 22:20:16 +0800

    build(micro-admin):Redis related configuration

---
+++ micro-admin/src/main/java/com/jacob/micro/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.jacob.micro.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Jacob
+ * @Description: Redis相关配置
+ * @Date: 2024/3/26 22:17
+ * @Version: 1.0
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig{
+}
