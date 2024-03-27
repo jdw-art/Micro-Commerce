bdfad07663e3c2f95f5a02f3955bc06458366cf9
jdw-art <jdw17733356203@icloud.com>
2024-03-27 21:27:35 +0800

    build(resource):Resource and role access correspondence operation component

---
+++ micro-admin/src/main/java/com/jacob/micro/component/ResourceRoleRulesHolder.java
@@ -0,0 +1,14 @@
+package com.jacob.micro.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Jacob
+ * @Description: 资源与角色访问对应关系操作组件
+ * @Date: 2024/3/27 21:23
+ * @Version: 1.0
+ */
+@Component
+public class ResourceRoleRulesHolder {
+
+}
