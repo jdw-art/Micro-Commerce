ac9d8689c19b3ac58a1b00a27cb49a73f65b0439
jdw-art <jdw17733356203@icloud.com>
2024-04-01 22:22:33 +0800

    build(productCategory):Add parameters for updating product categories

---
+++ micro-admin/src/main/java/com/jacob/micro/dto/PmsProductCategoryParam.java
@@ -0,0 +1,45 @@
+package com.jacob.micro.dto;
+
+import com.jacob.micro.validator.FlagValidator;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+/**
+ * @Author: Jacob
+ * @Description: 添加更新产品分类的参数
+ * @Date: 2024/4/1 22:10
+ * @Version: 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PmsProductCategoryParam {
+    @ApiModelProperty("父分类的编号")
+    private Long parentId;
+    @NotEmpty
+    @ApiModelProperty(value = "商品分类名称",required = true)
+    private String name;
+    @ApiModelProperty("分类单位")
+    private String productUnit;
+    @FlagValidator(value = {"0","1"},message = "状态只能为0或1")
+    @ApiModelProperty("是否在导航栏显示")
+    private Integer navStatus;
+    @FlagValidator(value = {"0","1"},message = "状态只能为0或1")
+    @ApiModelProperty("是否进行显示")
+    private Integer showStatus;
+    @Min(value = 0)
+    @ApiModelProperty("排序")
+    private Integer sort;
+    @ApiModelProperty("图标")
+    private String icon;
+    @ApiModelProperty("关键字")
+    private String keywords;
+    @ApiModelProperty("描述")
+    private String description;
+    @ApiModelProperty("产品相关筛选属性集合")
+    private List<Long> productAttributeIdList;
+}
